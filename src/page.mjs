bcc13a5b2be5aa6fdbf70095ab84308dde2410b9
Seth Battin <seth.battin@gmail.com>
2022-01-01 23:16:24 -0600

    produce output html in docs with a script

--- src/page.mjs
+++ src/page.mjs
@@ -16,9 +16,10 @@ function layout( htmlBody, meta) {
 
 
 // load a file and return { markup: string, meta: object}
-export async function page(filename, options) {
+export async function parse(filename, options) {
   const mdContent = await readFile(filename, 'utf-8')
   const tokens = marked.lexer(mdContent)
+  const heading = tokens.find(({type, depth}) => type === 'heading' && depth === 1)
   const content = marked.parser(tokens)
-  return {markup: layout(content, {title: filename}), meta: {tokens}}
+  return {markup: layout(content, {title: heading.text}), meta: {tokens}}
 }
