e49952564f85e360de392d09ac7570d924552a5f
Seth Battin <seth.battin@gmail.com>
2022-01-17 18:26:34 -0600

    correct media query

--- playwright/homepage.spec.ts
+++ playwright/homepage.spec.ts
@@ -4,6 +4,7 @@ import { test } from './breakpoints-test';
 test.describe('homepage', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('/');
+    await page.waitForLoadState('networkidle');
   });
   test('visual', async ({ page, width }) => {
   expect(await page.screenshot()).toMatchSnapshot(`homepage-${width}.png`);
