c5b16e94c41124391fd8255d0e485ef73a1870a5
Seth Battin <seth.battin@gmail.com>
2022-02-05 23:02:36 -0600

    generate tag markdown

--- scripts/publish.mjs
+++ scripts/publish.mjs
@@ -1,7 +1,8 @@
 import { parse } from "../src/page.mjs"
+import { addLink } from "../src/tag.mjs"
 import assert from "assert"
 import { join, relative, parse as pathparse } from "path"
-import { promises as fs } from "fs"
+import { promises as fs, constants as fsConstants } from "fs"
 import { exec as _exec } from "child_process"
 import { promisify } from "util"
 const exec = promisify(_exec)
@@ -10,8 +11,10 @@ const [nodeBin, script, fileName] = process.argv
 
 assert(!!fileName, "specify a markdown file to publish")
 
+const INDEX = 'index.html'
+
 const postFile = pathparse(relative('posts', fileName))
-const outFile = join('docs', postFile.dir.replace('_private', ''), postFile.name, "index.html")
+const outFile = join('docs', postFile.dir.replace('_private', ''), postFile.name, INDEX)
 Promise.all([
   fs.readFile(fileName, 'utf-8'),
   exec(`git log --reverse --date=short --format=%ad -- ${outFile}`)
@@ -21,7 +24,33 @@ Promise.all([
   await fs.mkdir(pathparse(outFile).dir, {recursive: true})
   await fs.writeFile(outFile, markup)
   console.log(`read markdown from ${fileName} and wrote to ${outFile}.`)
+  const tagUpdates = Object.keys(meta.categories).map(catName =>
+    updateTagMarkdown(catName, outFile, meta.title)
+  )
+  const tagList = await Promise.all(tagUpdates)
+  console.log(tagList)
 }).catch(e => {
   console.error(e);
   process.exit(1)
 })
+
+const updateTagMarkdown = async (name, articleOut, title) => {
+  const tagFile = join('tags', `${name}.md`)
+  const href = articleOut.replace('docs', '').replace(INDEX, '')
+  let tagFileContent = ''
+  let created = false
+  try {
+    await fs.access(tagFile).then(async () => { 
+      tagFileContent = await fs.readFile(tagFile, 'utf-8')
+    })
+  } catch (e) {
+    tagFileContent = `# ${name}\n\n`
+    created = true
+  }
+  const newContent = addLink(tagFileContent, { title, href })
+  const updated = !created && newContent !== tagFileContent
+  await fs.writeFile(tagFile, newContent, 'utf-8')
+
+  //TODO: publish the tag file
+  return { created, updated, href }
+}
---
+++ src/tag.mjs
@@ -0,0 +1,16 @@
+import { marked } from "marked"
+
+export function addLink(mdContent, link) {
+  console.log({mdContent, link})
+  const tokens = marked.lexer(mdContent)
+  const name = link.href.split('/').filter(Boolean).pop()
+  const text = tokens.reduce((acc, token) => acc + token.raw, '')
+  if (!Object.values(tokens.links).find(l => l.href === link.href)){
+    tokens.links = {
+      [name]: link,
+      ...tokens.links
+    }
+  }
+  const links = Object.entries(tokens.links).map(([key, l]) => `[${key}]: ${l.href} "${l.title}"`)
+  return `${text}${links.join('\n')}\n`
+}
