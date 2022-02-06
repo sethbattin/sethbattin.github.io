2fe49a376262ddb19ad6e7ae2002860571569466
Seth Battin <seth.battin@gmail.com>
2022-02-05 19:11:59 -0600

    add categories to meta

--- src/page.mjs
+++ src/page.mjs
@@ -22,6 +22,27 @@ function nowShortDate() {
 const getPublishedTokens = (shortDate) =>
   marked.lexer(pubdate(shortDate))
 
+/**
+ * use the marked internal `tokens.links` to find tags based on url convention.
+ * For nested tags, create parent links as well
+ */
+const getCategories = (tokens) =>
+  Object.values(tokens.links).reduce((acc, link) => {
+    const list = (link.href.indexOf('/tag/') !== 0) ? [] :
+      link.href.split('/').filter(s => !!s && s !== 'tag')
+    const links = list.reduce((acc2, l) => {
+      return {
+        [l]: `/tag/${[...Object.keys(acc2), l].join('/')}/`,
+        ...acc2,
+      }
+    }, {})
+    return {
+      ...acc,
+      ...links,
+    }
+  }, {})
+
+
 // parse markdown and return { markup: string, meta: object}
 export function parse(mdContent, _options = { }) {
   const options = Object.assign({}, defaults, _options)
@@ -34,7 +55,9 @@ export function parse(mdContent, _options = { }) {
     ({type, depth}, i) => (type === 'heading' && depth === 1) ? (headingPos = i, true) : false
   )
   tokens.splice(headingPos + 1, 0, ...publishedTokens)
-  const meta = { ...options, tokens, publishedDate: shortDate, title: heading.text }
+  const categories = getCategories(tokens)
+  console.log({categories})
+  const meta = { ...options, tokens, publishedDate: shortDate, title: heading.text, categories }
   const markup = options.layout(meta)
   return {markup, meta}
 }
