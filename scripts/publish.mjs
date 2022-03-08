89c1a4a416110d44a047ee64366fbc8472a645b2
Seth Battin <seth.battin@shipt.com>
2022-03-07 23:24:10 -0600

    good enough for a start

--- scripts/publish.mjs
+++ scripts/publish.mjs
@@ -57,9 +57,64 @@ const addRss = async (articleOut, meta) => {
   const href = articleHref(articleOut)
   const currentRss = await fs.readFile(join(WEBROOT, 'rss.xml'), 'utf-8').then(t => t.replace(/ *\n */g, ''))
   const doc = new xmldoc.XmlDocument(currentRss)
-  const docString = '<?xml version="1.0"?>' + doc.toString({compressed: true})
-  console.log({href, currentRss, doc, docString})
-  assert(currentRss === docString, 'oh no mismatch')
+  const docString = '<?xml version="1.0"?>' + doc.toString({compressed: false})
+
+  const childStrings = doc.firstChild.children.map(c => c.name).join('\n')
+
+  const { title, publishedDate, categories } = meta
+  const link = `//blog.seth.how${href}`
+  const lastBuildDate = (new Date()).toGMTString()
+  const [category, categoryLink] = Object.entries(categories).shift() || []
+
+  const items = doc.firstChild.childrenNamed('item')
+  console.log
+
+  const exists = items.find(i => i.valueWithPath('title') === title)
+
+  if (exists){
+    console.log('found existing rss entry; skipping')
+    return false
+  }
+
+  const newItem = `<item>
+  <title>${title}</title>
+  <link>${link}</link>
+  <pubDate>${(new Date(publishedDate)).toGMTString()}</pubDate>
+  <guid>${title}</guid>
+${category ? `  <category domain="${categoryLink.toLowerCase()}">${category}</category>
+` : ''}</item>`
+
+  const outRss = `<?xml version="1.0"?>
+<rss version="2.0">
+  <channel>
+    <title>Seth.How Blog</title>
+    <link>//blog.seth.how/</link>
+    <description>Blogs from Seth</description>
+    <language>en-us</language>
+    <pubDate>${lastBuildDate}</pubDate>
+    <lastBuildDate>${lastBuildDate}</lastBuildDate>
+    <generator>https://www.npmjs.com/package/xmldoc and https://github.com/sethbattin/sethbattin.github.io</generator>
+    <copyright>The automatic legal kind from originality and fixation, The Year of Our Lord ${(new Date()).getFullYear()}</copyright>
+    <managingEditor>me@seth.how</managingEditor>
+${newItem}
+${items.map(i => i.toString()).join('\n')}
+  </channel>
+</rss>`
+
+await fs.writeFile(join(WEBROOT, 'rss.xml'), outRss, 'utf-8')
+
+/*  console.log({
+    link,
+    out,
+    lastBuildDate,
+    
+ //   meta,
+  //  currentRss, 
+    fc: doc.firstChild,
+//    docString,
+    childStrings,
+  })
+//  assert(currentRss === docString, 'oh no mismatch')*/
 }
 
 const updateTagMarkdown = async (name, articleOut, title) => {
